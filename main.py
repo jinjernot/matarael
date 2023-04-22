33ebeb09577e034de8f40c6a722dd346c703b645
norindes <norindes@gmail.com>
2023-04-22 15:43:44 -0600

    added PRISM reports from ramiel

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import glob
 import openpyxl
 from openpyxl.styles import PatternFill
 from flask import Flask, request, render_template,redirect,send_file
@@ -9,6 +8,8 @@ app.use_static_for = 'static'
 
 ALLOWED_EXTENSIONS = {'xlsx'}
 
+
+### SCS check"""
 def allowed_file(filename):
     """set the allowed file.xlsx"""
     return '.' in filename and \
@@ -19,19 +20,28 @@ def allowed_file(filename):
 def upload_file():
     """Check if a file is .xlsx, if not, return an error"""
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        
-        file = request.files['file']
+        if 'file' in request.files:
+            file = request.files['file']
+            if allowed_file(file.filename):
+                cleanReport(file)
+                return send_file('SCS_QA.xlsx', as_attachment=True)
 
-        if not allowed_file(file.filename):
-            return render_template('error.html')
-        cleanReport(file)
-        
+        elif 'Summary' in request.files:
+            file = request.files['Summary']
+            if allowed_file(file.filename):
+                cleanSummary(file)
+                return send_file('Summary.xlsx', as_attachment=True)
 
-        return send_file('chido.xlsx', as_attachment=True)
-    
+        elif 'Report' in request.files:
+            file = request.files['Report']
+            if allowed_file(file.filename):
+                cleanSummary(file)
+                return send_file('Report.xlsx', as_attachment=True)
+
+        return render_template('error.html')
+
     return render_template('matarael.html')
+
 
 def cleanReport(file):
     """Load, Clean and create a a new xlsx file"""
@@ -861,6 +871,59 @@ def cleanReport(file):
     for cell in worksheet[1]:
         cell.fill = header_fill
     workbook.save('chido.xlsx')
+
+    return
+
+def cleanSummary(file):
+    """clean the file"""
+    df = pd.read_excel(file) #load the file
+    df.drop(index=range(5), inplace=True) #remove the first 5 rows
+    df = df.rename(columns=df.iloc[0]).drop(df.index[0])
+
+    split_string = lambda x: '/'.join(x.split('/')[2:]) if x and isinstance(x, str) and len(x.split('/')) >= 2 else x #clean up the string
+    df['ContainerName'] = df['ContainerName'].apply(split_string)#split the string
+
+    df['Tag'] = df['ContainerName'].str.extract('\[(.*?)\]', expand=False) #create new column with the tag
+    df['ContainerName'] = df['ContainerName'].str.replace('\[.*?\]', '', regex=True) #clean the tag
+
+    idx_chunk = (df.iloc[0] == 'ChunkValue').values #search for the columns chunk and M to swap
+    idx_m = (df.iloc[0] == 'M').values
+    df.loc[:, idx_chunk], df.loc[:, idx_m] = df.loc[:, idx_m].values, df.loc[:, idx_chunk].values
+
+    nan_columns = df.columns[pd.isna(df.columns)].tolist() #look for NaN headers
+    df = df.drop(columns=nan_columns)#remove columns with NaN header
+
+    first_col = df.iloc[:, 0] #move last column to the second position
+    last_col = df.iloc[:, -1]
+    middle_cols = df.iloc[:, 1:-1]
+    new_df = pd.concat([first_col, last_col, middle_cols], axis=1)
+    writer = pd.ExcelWriter(file, engine='xlsxwriter') #create a writer object
+    new_df.to_excel("Summary.xlsx", sheet_name="oli", index=False) #create the excel
+  
+    return
+
+def cleanExport(file):
+    df = pd.read_excel(file) #load the file
+    cols_to_drop = ['Length', 'Definition', 'Example', 'Format', 'Business Rule']
+    cols_to_drop.extend([col for col in df.columns if col.startswith('[Model')])
+    df = df.drop(cols_to_drop, axis=1)
+    df = df.drop([0, 1, 2])
+
+    new_column2 = df['ContainerName'].str.split('/', n=1, expand=True)[1].str.split('/', n=1, expand=True)[0]
+    df.insert(loc=0, column='root2', value=new_column2)
+
+    new_column = df['ContainerName'].str.split('/', n=1, expand=True)[0]
+    df.insert(loc=0, column='root1', value=new_column)
+
+    df['ContainerName'] = df['ContainerName'].str.split('/', n=2, expand=True)[2]
+
+    container_type = df.pop('ContainerType')
+    df.insert(loc=0, column='ContainerType', value=container_type)
+
+    df.to_excel("Report.xlsx", index=False)
+
+    return
+
 
 def main():
     upload_file()
