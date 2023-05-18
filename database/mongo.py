8368b99718b18d992ae885cccfa0a1d6174d0d45
norindes <norindes@gmail.com>
2023-05-17 22:16:59 -0600

    pyanywhere arguments

--- database/mongo.py
+++ database/mongo.py
@@ -6,7 +6,8 @@ def connect():
     pw = passwords.mongodbPassword
     connection_string = "mongodb+srv://norindes:{}@jinjernot.mhqetpj.mongodb.net/".format(pw)
     
-    client = pymongo.MongoClient(connection_string)
+    client = pymongo.MongoClient(connection_string, connectTimeoutMS=30000, socketTimeoutMS=None, connect=False, maxPoolsize=1
+)
     db = client.get_database("matarael")
     collection = db.get_collection("techspecs")
     
