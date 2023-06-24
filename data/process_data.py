a4c2ab39935a28866e49038fedeffbb7d524b3e2
jinjernot <norindes@gmail.com>
2023-06-24 00:20:38 -0600

    vector test 1

--- data/process_data.py
+++ data/process_data.py
@@ -1,29 +1,33 @@
 import json
+import pandas as pd
 
 def processData(json_path, container_name, container_df, df):
-
     # Open the JSON file and load the data into a dictionary.
     with open(json_path, 'r', encoding='utf-8') as f:
-
         data = json.load(f)
-        # Create an empty dictionary to store the accuracy of each container.
-        container_accuracy_dict = {}
 
-        # For each container in the JSON file, create a mask that filters the `container_df` DataFrame to only include rows where the `PhwebDescription` column contains the container's `PhwebDescription` value and the `ContainerValue` column contains the container's `ContainerValue` value.
-        for container in data[container_name]:
-            maskContainer = (container_df['PhwebDescription'].str.contains(container['PhwebDescription']) & \
-                        (container_df['ContainerValue'].str.contains(container['ContainerValue'], case=False, regex=False))) 
-            for idx in container_df[maskContainer].index:
-                container_accuracy_dict[idx] = f'SCS {container_name} OK'
-        
-        # For each index in the `container_df` DataFrame, if the index is not in the `container_accuracy_dict` dictionary, add the index to the dictionary with the value `ERROR: ` + `container_name`.
-        for idx in container_df.index:
-            if idx not in container_accuracy_dict:
-                container_accuracy_dict[idx] = f'ERROR: {container_name}'
+    # Convert the list of containers into a DataFrame.
+    container_data = pd.DataFrame(data[container_name])
 
-        # Update the `Accuracy` column of the `container_df` DataFrame with the values from the `container_accuracy_dict` dictionary.
-        container_df['Accuracy'] = container_df.index.map(container_accuracy_dict)
+    # Create an empty dictionary to store the accuracy of each container.
+    container_accuracy_dict = {}
 
-    # Update the `Accuracy` column of the `df` DataFrame with the values from the `container_df` DataFrame.
-    df.update(container_df['Accuracy'])
+    # Iterate over each container in the JSON data.
+    for container in container_data.itertuples(index=False):
+        # Create a mask using vectorized string operations.
+        maskContainer = (container_df['PhwebDescription'].str.contains(container.PhwebDescription) &
+                         container_df['ContainerValue'].str.contains(container.ContainerValue, case=False, regex=False))
+
+        # Update the `container_accuracy_dict` dictionary using boolean indexing.
+        container_accuracy_dict.update(container_df[maskContainer].index.to_series().map(lambda idx: (idx, f'SCS {container_name} OK')))
+
+    # Update the `container_accuracy_dict` dictionary with error values.
+    container_accuracy_dict.update(container_df[~container_df.index.isin(container_accuracy_dict.keys())].index.to_series().map(lambda idx: (idx, f'ERROR: {container_name}')))
+
+    # Update the `Accuracy` column of the `container_df` DataFrame using boolean indexing.
+    container_df.loc[container_accuracy_dict.keys(), 'Accuracy'] = [value for _, value in container_accuracy_dict.values()]
+
+    # Update the `Accuracy` column of the `df` DataFrame using boolean indexing.
+    df.loc[container_df.index, 'Accuracy'] = container_df['Accuracy']
+
     return df
