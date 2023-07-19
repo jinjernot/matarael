b1c08807e9311df59a701b17ae501967d03f575e
jinjernot <norindes@gmail.com>
2023-07-18 22:08:42 -0600

    jsons updated

--- data/qa_data.py
+++ data/qa_data.py
@@ -10,27 +10,27 @@ def clean_report(file):
         #df = pd.read_excel(file, engine='openpyxl')
         df = pd.read_excel(file.stream, engine='openpyxl')
 
+        cols_to_drop = ['Option', 'Status', 'SKU_FirstAppearanceDate', 'SKU_CompletionDate', 'SKU_Aging', 'PhwebValue', 'ExtendedDescription', 'ComponentCompletionDate', 'ComponentReadiness', 'SKUReadiness']
+        df = df.drop(cols_to_drop, axis=1)
+
         df = df[df['ContainerValue'] != '[BLANK]']
+        df = df[df['ContainerName'] != '[BLANK]']
+
         df.replace('\u00A0', ' ', regex=True, inplace=True)
         df.loc[df['ContainerValue'].str.endswith(';'), 'ContainerValue'] = df['ContainerValue'].str.slice(stop=-1)
         df['PhwebDescription'] = df['PhwebDescription'].str.lstrip()
         df['ContainerValue'] = df['ContainerValue'].astype(str)
 
-        cols_to_drop = ['Option', 'Status', 'SKU_FirstAppearanceDate', 'SKU_CompletionDate', 'SKU_Aging', 'PhwebValue', 'ExtendedDescription', 'ComponentCompletionDate', 'ComponentReadiness', 'SKUReadiness']
-        df = df.drop(cols_to_drop, axis=1)
-
         with open('/home/garciagi/SCS_Tool/data/data.json', 'r') as json_file:
         #with open('data/data.json', 'r') as json_file:
             json_data = json.load(json_file)
-
         groups = json_data['Groups']
         filtered_rows = df[df.apply(lambda row: any(row['ComponentGroup'] == group['ComponentGroup'] and row['ContainerName'] in group['ContainerName'] for group in groups), axis=1)]
-
         rows_to_delete = df.index.difference(filtered_rows.index)
         df = df.drop(rows_to_delete)
 
         df[['Accuracy', 'Correct Value', 'Additional Information']] = ''
-
+        
         for x in os.listdir('/home/garciagi/SCS_Tool/json'):
         #for x in os.listdir('json'):
             if x.endswith('.json'):
