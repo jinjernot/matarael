7f7acab35874005781d2273f0266f3e32df37fe8
norindes <norindes@gmail.com>
2024-05-27 14:01:20 -0600

    comment the code

--- app/core/format_data.py
+++ app/core/format_data.py
@@ -2,24 +2,21 @@ from openpyxl.styles import PatternFill,Font
 
 import openpyxl
 
-def formate_data():
+def format_data():
+    """This function is used to to formate the data in the excel file, bold headers, adjust the column width and highlight the errors"""
 
-    # Load the workbook into memory.
-    wb = openpyxl.load_workbook('/home/garciagi/SCS_Tool/SCS_QA.xlsx')
-    #wb = openpyxl.load_workbook('SCS_QA.xlsx')
-    # Get the active worksheet.
+    # Load Workbook and get the active sheet
+    wb = openpyxl.load_workbook('/home/garciagi/SCS_Tool/SCS_QA.xlsx') # Server
+    #wb = openpyxl.load_workbook('SCS_QA.xlsx') # Local
     worksheet = wb.active
 
-    # Create a pattern fill with the color `0072C6` and the fill type `solid`.
+    # Bold and color the headers
     header_fill = PatternFill(start_color='0072C6', end_color='0072C6', fill_type='solid') 
-
-    # Apply the fill to all the cells in the first row of the worksheet.
     for cell in worksheet[1]:
         cell.fill = header_fill
     
     # Loop over all the columns in the worksheet.
     for column in worksheet.columns:
-
         # Get the maximum length of any value in the column.
         max_length = 0
         column_name = column[0].column_letter
@@ -29,10 +26,8 @@ def formate_data():
                     max_length = len(str(cell.value))
             except:
                 pass
-        
         # Calculate the adjusted width of the column.
         adjusted_width = (max_length + 2)
-
         # Set the width of the column to the adjusted width.
         worksheet.column_dimensions[column_name].width = adjusted_width
         worksheet.column_dimensions['H'].width = 100
@@ -41,16 +36,9 @@ def formate_data():
     # Loop over all the cells in column `I`.
     for cell in worksheet['I']:
         if 'ERROR' in str(cell.value):
-
             # Change the color of the font in the cell to red.
             font = cell.font
             cell.font = Font(color='FF0000', name=font.name, size=font.size)
-
-    #for row in worksheet.rows:
-    #    for cell in row:
-    #        # Add a border to each cell in the worksheet.
-    #        cell.border = Border(left=Side(border_style='thin', color='000000'), right=Side(border_style='thin', color='000000'), top=Side(border_style='thin', color='000000'), bottom=Side(border_style='thin', color='000000'))
-
     # Save the workbook to a file called `SCS_QA.xlsx`.
-    wb.save('/home/garciagi/SCS_Tool/SCS_QA.xlsx')
-    #wb.save('SCS_QA.xlsx')
+    wb.save('/home/garciagi/SCS_Tool/SCS_QA.xlsx') # Server
+    #wb.save('SCS_QA.xlsx') # Local
--- app/core/product_line.py
+++ app/core/product_line.py
@@ -1,9 +1,10 @@
 import json
 
 def pl_check(df):
-    # Read JSON data from the external file
-    with open("/home/garciagi/SCS_Tool/app/core/data/product_lines.json", "r") as json_file:
-    #with open('app/core/data/product_lines.json', 'r') as json_file:
+    
+    # Read JSON data
+    with open("/home/garciagi/SCS_Tool/app/core/data/product_lines.json", "r") as json_file: # Server
+    #with open('app/core/data/product_lines.json', 'r') as json_file: # Local
         json_data = json.load(json_file)
 
     # Iterate through each product line in the JSON
@@ -14,12 +15,11 @@ def pl_check(df):
         # Check if the PL column in the DataFrame matches the current PL from the JSON
         matching_rows = df[df["PL"] == pl]
 
-
         # Iterate through ContainerName columns
         for container_name in container_names:
-            # Check if the "Container Value" column is empty in the matching rows
+            # Check if the 'Container Value' column is empty in the matching rows
             empty_container_rows = matching_rows[matching_rows["ContainerValue"].isna()]
-            # Update the "Accuracy" column for empty container rows
+            # Update the 'Accuracy' column for empty container rows
             df.loc[empty_container_rows.index, "ContainerValue"] = "ERROR: Mandatory Container Value"
 
     return df
